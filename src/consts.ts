d21bffb7642b99aeffd0a5fa97c42108a5b31be1
Mustafa Tunc <mustafatunc1998@gmail.com>
2026-01-29 13:34:58 +0300

    Update site title and description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Blogolog';
+export const SITE_DESCRIPTION = 'Merak ettiğin konularda anlaşılır, pratik ve güncel içerikler. Blogolog, SEO ve içerik denemeleriyle büyüyen bir bilgi arşivi.';
